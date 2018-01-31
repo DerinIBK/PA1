633302b35bd0d3a25f1494cd7d4558bed05639e3
unknown <adamdxn97@gmail.com>
2018-01-31 15:41:21 -0800

    block class finished

--- block.cpp
+++ block.cpp
@@ -1,20 +1,63 @@
 #include "block.h"
+#include <vector>
+#include <cmath>
 
+// Return the width of a block
 int Block::width() const{
-   /* your code here */
-   return 0;
-}
-int Block::height() const{
-   /* your code here */
-   return 0;
+   return data.size();
 }
 
+// Return the height of a block
+int Block::height() const{
+   return data.at(0).size();
+}
+
+
+// Draws the current block at position (upLeftX,upLeftY) in im.
+// Draw means replace the pixels
 void Block::render(PNG & im, int upLeftX, int upLeftY) const {
-   /* your code here */
+
+   // Iterate through im, starting from a certain corner
+   for (unsigned x = 0; x < width(); x++){
+       for(unsigned y = 0; y < height(); y++){
+
+            // The individual pixel of the image that we will be modifying
+            HSLAPixel *pixel = im.getPixel(x+upLeftX, y+upLeftY);
+
+            // Block pixel
+            HSLAPixel blockPixel = data[x][y];
+
+            // now pointing to the blocks values for saturation, luminance, hue and alpha
+            pixel->s = blockPixel.s;
+            pixel->l = blockPixel.l;
+            pixel->h = blockPixel.h;
+            pixel->a = blockPixel.a;
+            
+        }
+    }
    return;
 }
 
-void Block::build(PNG & im, int upLeftX, int upLeftY, int cols, int rows) {
-   /* your code here */
+// Grab the block data and store it into the data vector
+// You can find the data vector in the block.h file
+// From im, grabs the rows by cols block of pixels whose upper left corner is position (upLeftX,upLeftY).
+// (0,0) is the upper left position of the image
+void Block::build(PNG & im, int upLeftX, int upLeftY, int cols, int rows) {  
+
+    // Iterate through the section of the image you are examining right now
+    for (unsigned x = upLeftX; x < cols + upLeftX; x++) {
+
+        // Declare a vector of HSLAPixel's
+        vector<HSLAPixel> vector;
+        
+        for (unsigned y = upLeftY; y < rows + upLeftY; y++) {
+            // Get the pixel at point (x,y)
+            HSLAPixel *pixel = im.getPixel(x, y);
+            // Put that pixel's value in the vector (dereference)
+            vector.push_back(*pixel);
+        }
+        // Add the vector to the back of data vector
+        data.push_back(vector);
+    }
    return;
 }
